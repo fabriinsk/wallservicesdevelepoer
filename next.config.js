936cc0f5b39c7266c9551a82ec1404156684b342
fabriinsk <contatopro96fps@hotmail.com>
2025-04-18 17:46:56 +0000

    adiciona configurações

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['via.placeholder.com'], // adicione domínios se usar imagens externas
+    },
+  };
+  
+  module.exports = nextConfig;
+  
